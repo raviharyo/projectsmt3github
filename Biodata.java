bf4ec15edbffdd30d85e86b50dc41e35cbd168ba
raviharyo <raffiiphone99@gmail.com>
2021-01-20 17:30:39 +0700

    commit

---
+++ Biodata.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+  
+      
+      Scanner input_bio=new Scanner (System.in);
+        System.out.println("Biodata Ta'imir Masjid");
+        System.out.println("----------------");
+     
+      
+        
+      
+      System.out.println("Nama = Ravi Haryo Baskoro");
+      System.out.println("Nim = 1920010195");
+        System.out.println("Kelas = 3SC5");
+        System.out.println("Jurusan = Software Enginering");
+        System.out.println("\n");
+        System.out.println("--- Print Biodata seminar----");
+	System.out.println("----------------");
+	System.out.println("Tiket Masuk");
+	System.out.println("----------------");
+	System.out.println("jam = 04.00");
+	System.out.println("Tempat = Masjid UI");
+	System.out.println("Nomor Tempat Duduk = 001-002");
+	System.out.println("Tema Seminar = pentingnya ilmu agama dalam penerapan kehidupan");
+    }
+    }
