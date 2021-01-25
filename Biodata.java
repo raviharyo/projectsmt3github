7294166985bff9c3530853a19de9b49c6a1be10a
raviharyo <raffiiphone99@gmail.com>
2021-01-25 13:41:43 +0700

    commitbaru25

--- Biodata.java
+++ Biodata.java
@@ -5,40 +5,40 @@ public class Biodata {
     public static void main(String[] args) {
   
       
-//      Scanner input_bio=new Scanner (System.in);
-//        System.out.println("Biodata Ta'imir Masjid");
-//        System.out.println("----------------");
-//     
-//      
-//        
-//      
-//      System.out.println("Nama = Ravi Haryo Baskoro");
-//      System.out.println("Nim = 1920010195");
-//        System.out.println("Kelas = 3SC5");
-//        System.out.println("Jurusan = Software Enginering");
-//        System.out.println("\n");
-//        System.out.println("--- Print Biodata seminar----");
-//	System.out.println("----------------");
-//	System.out.println("Tiket Masuk");
-//	System.out.println("----------------");
-//	System.out.println("jam = 04.00");
-//	System.out.println("Tempat = Masjid UI");
-//	System.out.println("Nomor Tempat Duduk = 001-002");
-//	System.out.println("Tema Seminar = pentingnya ilmu agama dalam penerapan kehidupan");
-        Scanner input= new Scanner(System.in);
-
-        int panjang, lebar, luas;
+      Scanner input_bio=new Scanner (System.in);
+        System.out.println("Biodata Ta'imir Masjid");
+        System.out.println("----------------");
+     
       
-        System.out.println("PROGRAM JAVA MENGHITUNG LUAS PERSEGI PANJANG : ");
-        System.out.println("-----------------------------------------------");
-
-        System.out.print("Masukan Panjang      : ");
-        panjang=input.nextInt();
-        System.out.print("Masukan Lebar        : ");
-        lebar=input.nextInt();
         
-        luas=panjang*lebar;
-
-        System.out.println("Luas Persegi Panjang : "+luas);
+      
+      System.out.println("Nama = Ravi Haryo Baskoro & Laksamana Zaidan");
+      System.out.println("Nim = 1920010195");
+        System.out.println("Kelas = 3SC5");
+        System.out.println("Jurusan = Software Enginering");
+        System.out.println("\n");
+        System.out.println("--- Print Biodata seminar----");
+	System.out.println("----------------");
+	System.out.println("Undangan pengajian");
+	System.out.println("----------------");
+	System.out.println("jam = 04.00");
+	System.out.println("Tempat = Masjid UI");
+	System.out.println("Nomor Tempat Duduk = 001-002");
+	System.out.println("Tema Seminar = pentingnya ilmu agama dalam penerapan kehidupan");
+//        Scanner input= new Scanner(System.in);
+//
+//        int panjang, lebar, luas;
+//      
+//        System.out.println("PROGRAM JAVA MENGHITUNG LUAS PERSEGI PANJANG : ");
+//        System.out.println("-----------------------------------------------");
+//
+//        System.out.print("Masukan Panjang      : ");
+//        panjang=input.nextInt();
+//        System.out.print("Masukan Lebar        : ");
+//        lebar=input.nextInt();
+//        
+//        luas=panjang*lebar;
+//
+//        System.out.println("Luas Persegi Panjang : "+luas);
     }
     }
