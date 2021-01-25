833f45f207cae53228cbee3eb97ff46c6ea7bbfa
raviharyo <raffiiphone99@gmail.com>
2021-01-25 13:51:54 +0700

    testtauto

--- Biodata.java
+++ Biodata.java
@@ -6,7 +6,7 @@ public class Biodata {
   
       
       Scanner input_bio=new Scanner (System.in);
-        System.out.println("Biodata Ta'imir Masjid");
+        System.out.println("Biodata Ta'imir Masjid push kedua ");
         System.out.println("----------------");
      
       
